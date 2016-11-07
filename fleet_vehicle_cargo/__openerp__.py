9177de2588801bcffa79269a45fe4695f3eb4e94
Andhitia Rama <andhitia.r@gmail.com>
2016-11-07 19:50:02 +0700

    [ADD][8.0] fleet_vehicle_cargo (#20)

---
+++ fleet_vehicle_cargo/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Vehicle Cargo Information",
+    "version": "8.0.1.0.0",
+    "category": "Fleet",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "fleet_vehicle_notebook",
+    ],
+    "data": [
+        "views/fleet_vehicle_views.xml",
+    ],
+}
