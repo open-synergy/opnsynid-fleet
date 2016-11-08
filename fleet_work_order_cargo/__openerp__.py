7944a6dbcd5a6190688a74ae3b332565d58a1198
Andhitia Rama <andhitia.r@gmail.com>
2016-11-08 08:28:30 +0700

    Module Structure

---
+++ fleet_work_order_cargo/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Fleet Work Order Cargo",
+    "version": "8.0.1.0.0",
+    "category": "Fleet",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "fleet_work_order",
+        "fleet_vehicle_cargo",
+        "stock_shipment_management",
+    ],
+    "data": [
+    ],
+}
