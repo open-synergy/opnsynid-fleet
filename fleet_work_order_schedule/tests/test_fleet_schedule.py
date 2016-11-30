f4e475c48bbe2c4a6df4409a783648928884cbc6
Andhitia Rama <andhitia.r@gmail.com>
2016-12-01 05:23:47 +0700

    Add unittest

--- fleet_work_order_schedule/tests/test_fleet_schedule.py
+++ fleet_work_order_schedule/tests/test_fleet_schedule.py
@@ -10,4 +10,35 @@ class TestFleetSchedule(TransactionCase):
     def setUp(self, *args, **kwargs):
         super(TestFleetSchedule, self).setUp(*args, **kwargs)
 
-        # TODO Replace this for something useful or delete this method
+        self.type3 = self.env.ref(
+            "fleet_work_order.type_3")
+
+    def test_schedule(self):
+        obj_schedule = self.env[
+            "fleet.work.order.type.schedule"]
+        obj_order = self.env[
+            "fleet.work.order"]
+        self.type3.action_add_schedule()
+
+        self.assertEqual(
+            len(self.type3.schedule_ids),
+            1)
+
+        before = obj_order.search_count(
+            [("type_id", "=", self.type3.id)])
+
+        obj_schedule.run_schedule(
+            self.type3.schedule_ids[0].id)
+
+        after = obj_order.search_count(
+            [("type_id", "=", self.type3.id)])
+
+        self.assertEqual(
+            after,
+            before + 1)
+
+        self.type3.schedule_ids[0].unlink()
+
+        
+
+
