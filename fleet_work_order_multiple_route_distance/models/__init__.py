62ba63846bc1425322887d65b824c37d3c29d35b
Andhitia Rama <andhitia.r@gmail.com>
2017-08-24 22:06:38 +0700

    [8.0.1.0.0] fleet_work_order_multiple_route_distance

---
+++ fleet_work_order_multiple_route_distance/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import fleet_route_template
+from . import fleet_work_order_type_route
+from . import fleet_route
