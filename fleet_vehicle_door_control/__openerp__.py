594c8a14cb71b8024d538578b0af823d04bee99c
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-13 21:15:19 +0700

    [8.0.1.0.0][fleet_vehicle_door_control]
    [8.0.1.0.0][fleet_vehicle_door_control] Revision

---
+++ fleet_vehicle_door_control/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Vehicle Door Control Management",
+    "version": "8.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "category": "Fleet",
+    "installable": True,
+    "depends": [
+        "fleet",
+        "proxy_backend"
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/proxy_backend_device_type_views.xml",
+        "views/vehicle_door_views.xml"
+    ],
+}
