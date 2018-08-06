1aca2a8d69bec4f2dac18fb97214a205545e43d8
mikevhe18 <viriyananda.michael@gmail.com>
2018-08-06 14:58:31 +0700

    [8.0.1.0.0][fleet_vehicle_door_control_raspberry_relay]
    Fix Travis Error

---
+++ fleet_vehicle_door_control_raspberry_relay/models/fleet_vehicle_door.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class FleetVehicleDoor(models.Model):
+    _inherit = "fleet.vehicle.door"
+
+    raspberry_relay_id = fields.Many2one(
+        string="Raspberry Relay",
+        comodel_name="proxy.backend_raspberry_relay"
+    )
+    device_raspberry_id = fields.Many2one(
+        string="Raspberry Relay",
+        related="raspberry_relay_id.device_id",
+        comodel_name="proxy.backend_device",
+        store=False
+    )
+    channel = fields.Integer(
+        string="Channel",
+        related="raspberry_relay_id.pin",
+        store=False
+    )
