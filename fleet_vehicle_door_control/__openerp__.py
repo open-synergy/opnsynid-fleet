f02178633d61a5015de986d99ba2880b098098c5
mikevhe18 <viriyananda.michael@gmail.com>
2018-08-15 18:35:36 +0700

    [8.0.1.2.0][fleet_vehicle_door_control]

--- fleet_vehicle_door_control/__openerp__.py
+++ fleet_vehicle_door_control/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Vehicle Door Control",
-    "version": "8.0.1.1.1",
+    "version": "8.0.1.2.0",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
@@ -16,6 +16,7 @@
     "data": [
         "security/ir.model.access.csv",
         "views/proxy_backend_device_type_views.xml",
-        "views/vehicle_door_views.xml"
+        "views/vehicle_door_views.xml",
+        "views/fleet_vehicle_views.xml"
     ],
 }
