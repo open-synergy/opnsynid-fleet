95196d477d1fdcabddc74f0bc7d5b40056de886b
Andhitia Rama <andhitia.r@gmail.com>
2018-08-21 10:05:58 +0700

    [8.0.1.1.0] fleet_work_order_passanger_ticket_sale
    * Add price_after_tax on fleet_work_order_passanger_analysis

---
+++ fleet_work_order_passanger_ticket_sale/reports/fleet_work_order_passanger_analysis.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class FleetWorkOrderPassangerAnalysis(models.Model):
+    _inherit = "fleet.work_order_passanger_analysis"
+
+    price_after_tax = fields.Float(
+        string="Price After Tax",
+        )
+
+
+    def _select(self):
+        _super = super(FleetWorkOrderPassangerAnalysis, self)
+        select_str = _super._select()
+        select_str += """
+            ,
+            SUM(a.price_subtotal) AS price_after_tax
+        """
+        return select_str
