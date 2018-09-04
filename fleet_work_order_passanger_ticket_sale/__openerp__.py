178be5ac6b3674f6f72af25e09bb69edb0ecb43d
mikevhe18 <viriyananda.michael@gmail.com>
2018-09-04 15:34:38 +0700

    [8.0.1.2.0][fleet_work_order_passanger_ticket_sale]

--- fleet_work_order_passanger_ticket_sale/__openerp__.py
+++ fleet_work_order_passanger_ticket_sale/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Passanger Ticket Sale",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "category": "Fleet",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
