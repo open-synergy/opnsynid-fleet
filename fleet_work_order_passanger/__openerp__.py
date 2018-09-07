b127007dfa091e20cc10ad473b3b68ddf894ac6a
Andhitia Rama <andhitia.r@gmail.com>
2018-09-07 08:28:03 +0700

    [8.0.1.7.2] fleet_work_order_passanger
    * [fleet_work_order_passanger] driver interface
    * [fleet_work_order_passanger] bump version
    * [fleet_work_order_passanger] add dependecy to hr

--- fleet_work_order_passanger/__openerp__.py
+++ fleet_work_order_passanger/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Passanger Manifest",
-    "version": "8.0.1.6.2",
+    "version": "8.0.1.7.2",
     "category": "Fleet",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
@@ -13,11 +13,13 @@
         "fleet_work_order",
         "fleet_vehicle_seat_number",
         "web_onchange_action",
-        "fleet_vehicle_door_control_raspberry_relay"
+        "fleet_vehicle_door_control_raspberry_relay",
+        "hr",
     ],
     "data": [
         "security/ir.model.access.csv",
         "data/ir_sequence_data.xml",
+        "data/ir_actions_server_data.xml",
         "menu.xml",
         "wizards/passanger_boarding_disembark_views.xml",
         "reports/fleet_work_order_passanger_analysis.xml",
