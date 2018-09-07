41382260d8d380068c74333278d88493d020cc64
Andhitia Rama <andhitia.r@gmail.com>
2018-09-07 08:29:51 +0700

    [8.0.1.3.0] fleet_work_order_passanger_ticket_sale
    * [fleet_work_order_passanger_ticket_sale] bump version
    * [fleet_work_order_passanger] Fix ticket counter kanban view

--- fleet_work_order_passanger_ticket_sale/__openerp__.py
+++ fleet_work_order_passanger_ticket_sale/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Passanger Ticket Sale",
-    "version": "8.0.1.2.0",
+    "version": "8.0.1.3.0",
     "category": "Fleet",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
