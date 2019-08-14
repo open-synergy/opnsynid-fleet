30f385bb801b8a691f71e5ed5ae3ad3100ca7dc0
mikevhe18 <viriyananda.michael@gmail.com>
2019-08-14 13:10:54 +0700

    Fixing error travis

--- fleet_work_order_multiple_route_gamification/models/fleet_route.py
+++ fleet_work_order_multiple_route_gamification/models/fleet_route.py
@@ -2,7 +2,7 @@
 # Copyright 2019 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from openerp import models, fields
 
 
 class FleetRoute(models.Model):
