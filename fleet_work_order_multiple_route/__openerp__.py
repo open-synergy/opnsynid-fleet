81159c54907a3a67fd62d61a4c30ad2fb3fad115
mikevhe18 <viriyananda.michael@gmail.com>
2019-08-14 10:20:17 +0700

    [8.0.1.1.0][fleet_work_order_multiple_route]
    Fixing Typo

--- fleet_work_order_multiple_route/__openerp__.py
+++ fleet_work_order_multiple_route/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Multiple Route on Fleet Work Order",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "Fleet",
     "website": "https://opensynergy-indonesia.com/",
     "author": "Andhitia Rama, OpenSynergy Indonesia",
--- fleet_work_order_multiple_route/models/fleet_work_order.py
+++ fleet_work_order_multiple_route/models/fleet_work_order.py
@@ -8,8 +8,11 @@ from openerp import models, fields, api
 class FleetWorkOrder(models.Model):
     _inherit = "fleet.work.order"
 
-    @api.one
-    @api.depends('multiple_route', 'route_ids')
+    @api.multi
+    @api.depends(
+        "multiple_route",
+        "route_ids"
+    )
     def _compute_route(self):
         if self.multiple_route and len(self.route_ids) > 0:
             self.function_start_location_id = self.route_ids[
@@ -29,71 +32,72 @@ class FleetWorkOrder(models.Model):
         inverse_name="order_id",
         readonly=True,
         states={
-            'draft': [('readonly', False)],
+            "draft": [("readonly", False)],
         },
     )
 
     multiple_route = fields.Boolean(
-        string='Multiple Routes',
+        string="Multiple Routes",
         readonly=True,
         states={
-            'draft': [('readonly', False)],
+            "draft": [("readonly", False)],
         },
     )
 
     function_start_location_id = fields.Many2one(
-        string='Start Location',
-        comodel_name='res.partner',
-        compute='_compute_route',
+        string="Start Location",
+        comodel_name="res.partner",
+        compute="_compute_route",
         store=True,
         readonly=True,
     )
 
     function_end_location_id = fields.Many2one(
-        string='End Location',
-        comodel_name='res.partner',
-        compute='_compute_route',
+        string="End Location",
+        comodel_name="res.partner",
+        compute="_compute_route",
         store=True,
         readonly=True,
     )
 
     function_distance = fields.Float(
-        string='End Location',
-        compute='_compute_route',
+        string="Distance",
+        compute="_compute_route",
         store=True,
         readonly=True,
     )
 
-    @api.onchange('type_id')
-    def onchange_type_id(self):
-        self.vehicle_id = False
-        self.driver_id = False
-        self.co_driver_id = False
+    @api.onchange(
+        "type_id"
+    )
+    def onchange_multiple_route(self):
+        if self.type_id:
+            wo_type = self.type_id
+            self.multiple_route = wo_type.multiple_route
+
+    @api.onchange(
+        "type_id"
+    )
+    def onchange_route_ids(self):
         self.route_ids.unlink()
         if self.type_id:
             wo_type = self.type_id
-            self.vehicle_id = wo_type.vehicle_id and \
-                wo_type.vehicle_id.id or False
-            self.driver_id = wo_type.driver_id and \
-                wo_type.driver_id.id or False
-            self.co_driver_id = wo_type.co_driver_id and \
-                wo_type.co_driver_id.id or False
-            self.start_location_id = wo_type.start_location_id and \
-                wo_type.start_location_id.id or False
-            self.end_location_id = wo_type.end_location_id and \
-                wo_type.end_location_id.id or False
-            self.distance = wo_type.distance
-            self.multiple_route = wo_type.multiple_route
             if wo_type.route_ids:
                 routes = []
                 for route in wo_type.route_ids:
-                    res = {
-                        'name': route.name,
-                        'sequence': route.sequence,
-                        'route_template_id': route.route_template_id.id,
-                        'start_location_id': route.start_location_id.id,
-                        'end_location_id': route.end_location_id.id,
-                        'distance': route.distance,
-                    }
+                    res = self._prepare_route_data(route)
                     routes.append((0, 0, res))
                 self.route_ids = routes
+
+    @api.multi
+    def _prepare_route_data(self, route):
+        self.ensure_one()
+        result = {
+            "name": route.name,
+            "sequence": route.sequence,
+            "route_template_id": route.route_template_id.id,
+            "start_location_id": route.start_location_id.id,
+            "end_location_id": route.end_location_id.id,
+            "distance": route.distance,
+        }
+        return result
