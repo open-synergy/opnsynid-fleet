7b978678d3f97602ad434858c13567fb888ad00e
mikevhe18 <viriyananda.michael@gmail.com>
2019-08-14 10:20:37 +0700

    [8.0.1.0.0][fleet_work_order_gamification]

---
+++ fleet_work_order_gamification/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import fleet_work_order_type
+from . import fleet_work_order
---
+++ fleet_work_order_gamification/models/fleet_work_order.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class FleetWorkOrder(models.Model):
+    _inherit = "fleet.work.order"
+
+    gamification_point = fields.Float(
+        string="Gamification Point",
+        default=1.0
+    )
+
+    @api.multi
+    @api.onchange(
+        "type_id"
+    )
+    def onchange_gamification_point(self):
+        if not self.type_id:
+            self.gamification_point = 1.0
+            return
+        self.gamification_point =\
+            self.type_id.gamification_point
