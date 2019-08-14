57d1cd044ec874bb96c6e8f6046b649498a80e19
mikevhe18 <viriyananda.michael@gmail.com>
2019-08-14 10:20:55 +0700

    [8.0.1.0.0][fleet_work_order_multiple_route_gamification]

---
+++ fleet_work_order_multiple_route_gamification/models/fleet_route.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class FleetRoute(models.Model):
+    _inherit = "fleet.route"
+
+    gamification_point = fields.Float(
+        string="Gamification Point",
+        default=1.0
+    )
---
+++ fleet_work_order_multiple_route_gamification/models/fleet_work_order.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class FleetWorkOrder(models.Model):
+    _inherit = "fleet.work.order"
+
+    gamification_point = fields.Float(
+        string="Gamification Point",
+        default=1.0
+    )
+
+    @api.multi
+    def _prepare_route_data(self, route):
+        _super = super(FleetWorkOrder, self)
+        res = _super._prepare_route_data(route)
+
+        res["gamification_point"] =\
+            route.gamification_point
+
+        return res
