ed71e6191970b60d45c5cb7025c5e1108db72173
Andhitia Rama <andhitia.r@gmail.com>
2020-09-14 16:35:42 +0700

    [fleet_work_order_cargo_from_picking] 8.0.1.2.0
    * Add pickings volume, weight, volume diff, and weight diff

--- fleet_work_order_cargo_from_picking/__openerp__.py
+++ fleet_work_order_cargo_from_picking/__openerp__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Fleet Work Order Cargo From Picking",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "category": "Fleet",
-    "website": "https://opensynergy-indonesia.com/",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id/",
+    "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
